4175fb829e40cacd881a94c993c429273a6b3aa5
joriesieck <jsieck@globalcognition.org>
2019-05-29 11:08:56 -0400

    added function to create table on plugin activation

--- db-plugin-abstraction.php
+++ db-plugin-abstraction.php
@@ -6,3 +6,35 @@ Description: A plugin that implements the database abstraction layer stuff with 
 Author: Jorie Sieck
 Author URI: https://my.thinkeracademy.com
 */
+
+global $db_version;
+$db_version = '1.0';
+
+// Call create_table on plugin activation.
+register_activation_hook(__FILE__,'create_table');
+/*
+ * Creates the table "wp_catalogue_pets" in the database.
+ */
+function create_table() {
+    global $wpdb;
+    global $db_version;
+    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+
+    $table_name = $wpdb->prefix . 'catalogue_pets';
+
+    $charset_collate = $wpdb->get_charset_collate();
+
+    $sql = "CREATE TABLE $table_name (
+        id mediumint(9) UNSIGNED NOT NULL AUTO_INCREMENT,
+		name tinytext NOT NULL,
+        type tinytext NOT NULL,
+        description longtext NOT NULL,
+        price bigint(20) NOT NULL,
+        PRIMARY KEY (id)
+	) $charset_collate;";
+
+    dbDelta($sql);
+    $success = empty( $wpdb->last_error );
+    update_option($table_name . '_db_version',$db_version);
+    return $success;
+}
