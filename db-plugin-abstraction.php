266dd5adbf4cca4d09fd6f9ce6690cdf74e0a62c
joriesieck <jsieck@globalcognition.org>
2019-05-29 10:23:27 -0400

    just an empty file

---
+++ db-plugin-abstraction.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Plugin Name: DB Plugin Abstraction
+Version: 1.0
+Description: A plugin that implements the database abstraction layer stuff with the same pet data
+Author: Jorie Sieck
+Author URI: https://my.thinkeracademy.com
+*/
